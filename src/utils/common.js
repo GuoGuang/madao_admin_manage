2720b14f7f7c3958e3201b47f19dff1dc30a6ec5
guoguang0536@gmail.com <guoguang0536@gmail.com>
2019-06-05 00:21:38 +0800

    临时添加公共转换时间函数

--- src/utils/common.js
+++ src/utils/common.js
@@ -35,6 +35,37 @@ export default {
     }
     return ''
   },
+  /**
+  * 供普通函数调用
+  * {{ common.dateFormat(profileInfo.createAt) }}
+  * @param row
+  * @param column
+  * @returns {string}
+  */
+  dateFormatFun(timestamp) {
+    if (timestamp != null) {
+      var d = new Date(timestamp)// 根据时间戳生成的时间对象
+      var yy = d.getFullYear() // 年
+      var mm = d.getMonth() + 1 // 月
+      var dd = d.getDate() // 日
+      var hh = d.getHours() // 时
+      var ii = d.getMinutes() // 分
+      var ss = d.getSeconds() // 秒
+      var clock = yy + '-'
+      if (mm < 10) clock += '0'
+      clock += mm + '-'
+      if (dd < 10) clock += '0'
+      clock += dd + ' '
+      if (hh < 10) clock += '0'
+      clock += hh + ':'
+      if (ii < 10) clock += '0'
+      clock += ii + ':'
+      if (ss < 10) clock += '0'
+      clock += ss
+      return clock
+    }
+    return ''
+  },
 
   /**
    * 将普通列表数据转换为树形结构
