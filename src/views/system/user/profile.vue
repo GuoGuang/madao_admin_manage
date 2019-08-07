639637a5d60792bd18cd0a0037919ab4b63dd013
guoguang0536@gmail.com <guoguang0536@gmail.com>
2019-08-07 17:04:34 +0800

    :art: optimize style

--- src/views/system/user/profile.vue
+++ src/views/system/user/profile.vue
@@ -113,26 +113,20 @@
     <!-- 上传头像模态框 -->
     <el-dialog :visible.sync="avatarDialog" title="修改头像" class="upload-pard">
       <el-tabs v-model="activeName" type="card" @tab-click="handleTabClick">
-        <el-tab-pane label="个性头像" name="1">
+        <el-tab-pane label="个性头像" name="1" class="diy-avatar">
           <el-row>
-            <el-col :span="20">
-              <ul class="demo">
-                <li v-for="avatar in defaultAvatars" :key="avatar">
-                  <el-image
-                    :src="avatar"
-                    style="width: 100px; height: 100px"
-                    @click="defaultAvatarHandle(avatar)"/>
-                </li>
-              </ul>
+            <el-col :span="18" class="col-avatar-list" >
+              <el-image
+                v-for="avatar in defaultAvatars"
+                :key="avatar"
+                :src="avatar"
+                @click="defaultAvatarHandle(avatar)"/>
             </el-col>
 
-            <el-col :span="4" style="margin-top: 1em;">
-              <div>
-                <el-image
-                  :src="defaultAvatar"
-                  style="width: 100px; height: 100px"/>
-                <p style="padding-left: 20px;">头像预览</p>
-              </div>
+            <el-col :span="5" class="col-avatar-preview" >
+              <el-image
+                :src="defaultAvatar"/>
+              <p >头像预览</p>
             </el-col>
           </el-row>
 
@@ -198,12 +192,23 @@ export default {
       isSelected: false, // 是否已选择文件
       profileInfo: {},
       currentTabName: '1', // 当前默认tab name
-      defaultAvatar: 'https://vue-admin-guoguang.oss-cn-shanghai.aliyuncs.com/a.png',
+      defaultAvatar: 'http://vue-admin-guoguang.oss-cn-shanghai.aliyuncs.com/icode/image/avatar/17e420c250804efe904a09a33796d5a10.jpg',
       defaultAvatars: [
-        'https://vue-admin-guoguang.oss-cn-shanghai.aliyuncs.com/a.png',
-        'https://vue-admin-guoguang.oss-cn-shanghai.aliyuncs.com/b.png',
-        'https://vue-admin-guoguang.oss-cn-shanghai.aliyuncs.com/c.png',
-        'https://vue-admin-guoguang.oss-cn-shanghai.aliyuncs.com/d.png'
+        'http://vue-admin-guoguang.oss-cn-shanghai.aliyuncs.com/icode/image/avatar/17e420c250804efe904a09a33796d5a10.jpg',
+        'http://vue-admin-guoguang.oss-cn-shanghai.aliyuncs.com/icode/image/avatar/17e420c250804efe904a09a33796d5a16.jpg',
+        'http://vue-admin-guoguang.oss-cn-shanghai.aliyuncs.com/icode/image/avatar/1d22f3e41d284f50b2c8fc32e0788698.jpeg',
+        'http://vue-admin-guoguang.oss-cn-shanghai.aliyuncs.com/icode/image/avatar/2dd7a2d09fa94bf8b5c52e5318868b4d9.jpg',
+        'http://vue-admin-guoguang.oss-cn-shanghai.aliyuncs.com/icode/image/avatar/2dd7a2d09fa94bf8b5c52e5318868b4df.jpg',
+        'http://vue-admin-guoguang.oss-cn-shanghai.aliyuncs.com/icode/image/avatar/496b3ace787342f7954b7045b8b06804.jpeg',
+        'http://vue-admin-guoguang.oss-cn-shanghai.aliyuncs.com/icode/image/avatar/595ba7b05f2e485eb50565a50cb6cc3c.jpeg',
+        'http://vue-admin-guoguang.oss-cn-shanghai.aliyuncs.com/icode/image/avatar/5997fedcc7bd4cffbd350b40d1b5b9824.jpg',
+        'http://vue-admin-guoguang.oss-cn-shanghai.aliyuncs.com/icode/image/avatar/5997fedcc7bd4cffbd350b40d1b5b987.jpg',
+        'http://vue-admin-guoguang.oss-cn-shanghai.aliyuncs.com/icode/image/avatar/87d8194bc9834e9f8f0228e9e530beb1.jpeg',
+        'http://vue-admin-guoguang.oss-cn-shanghai.aliyuncs.com/icode/image/avatar/8f5b60ef00714a399ee544d331231820.jpeg',
+        'http://vue-admin-guoguang.oss-cn-shanghai.aliyuncs.com/icode/image/avatar/964e40b005724165b8cf772355796c8c.jpeg',
+        'http://vue-admin-guoguang.oss-cn-shanghai.aliyuncs.com/icode/image/avatar/a3b10296862e40edb811418d64455d00.jpeg',
+        'http://vue-admin-guoguang.oss-cn-shanghai.aliyuncs.com/icode/image/avatar/a43456282d684e0b9319cf332f8ac468.jpeg',
+        'http://vue-admin-guoguang.oss-cn-shanghai.aliyuncs.com/icode/image/avatar/bba284ac05b041a8b8b0d1927868d5c9x.jpg'
       ],
       activeName: '1',
       account: this.$store.getters.account,
@@ -352,11 +357,11 @@ export default {
 
 <style el="stylesheet/scss" lang="scss">
 .profile {
-   --upload-avatar-display: grid;
-  .el-upload{
+  --upload-avatar-display: grid;
+  .el-upload {
     display: grid;
   }
-  .upload-avatar >.el-upload--picture-card{
+  .upload-avatar > .el-upload--picture-card {
     display: var(--upload-avatar-display);
   }
 }
@@ -439,21 +444,40 @@ export default {
   .text-warning {
     color: #f90;
   }
-  .upload-pard{
-    .upload-avatar{
+  .upload-pard {
+    .upload-avatar {
       padding-left: 2em;
     }
   }
 }
-
-.demo{
-      display: flex;
-    justify-content: space-around;
-        padding-left: 0;
-            border: 2px dashed #eee;
-  li{
-     list-style: none;
-         cursor: pointer;
+</style>
+<style rel="stylesheet/scss" lang="scss">
+.profile {
+  .upload-pard {
+    .diy-avatar {
+      .col-avatar-list {
+        border-right: 1px solid #dcdfe6;
+        img {
+          margin: 0.5rem;
+          width: 100px;
+          height: 100px;
+        }
+      }
+      .col-avatar-preview {
+        margin-top: 1em;
+        padding-left: 1em;
+        .el-image {
+          box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+        }
+        img {
+          width: 100px;
+          height: 100px;
+        }
+        p {
+          padding-left: 20px;
+        }
+      }
+    }
   }
 }
 </style>
