75fe81e8f8ec125e85fe7012d5441fbd495cb536
guoguang0536@gmail.com <guoguang0536@gmail.com>
2019-08-09 13:54:58 +0800

    :sparkles: Add tool of time

--- src/utils/index.js
+++ src/utils/index.js
@@ -68,6 +68,26 @@ export function formatTime(time, option) {
     )
   }
 }
+export function cueerntPeriodsFormat(now) {
+  const hour = now.getHours()
+  if (hour < 6) {
+    return '凌晨好'
+  } else if (hour < 9) {
+    return '早上好'
+  } else if (hour < 12) {
+    return '上午好'
+  } else if (hour < 14) {
+    return '中午好'
+  } else if (hour < 17) {
+    return '下午好'
+  } else if (hour < 19) {
+    return '傍晚好'
+  } else if (hour < 22) {
+    return '晚上好'
+  } else {
+    return '夜里好'
+  }
+}
 
 // 格式化时间
 export function getQueryObject(url) {
