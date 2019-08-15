d4c611664f757b24617f9575d41951a750f94cd0
guoguang0536@gmail.com <guoguang0536@gmail.com>
2019-08-15 19:44:02 +0800

    :wrench: Update domain interface

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,7 @@
 // production 生产环境
+// 部署 https://juejin.im/post/5c4a6fcd518825469414e062
 module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"prod"',
-  BASE_API: '"https://api-prod"'
+  BASE_API: '"https://api-ucode.im"'
 }
