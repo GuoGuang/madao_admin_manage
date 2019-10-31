4df7e92e61b958a4c4db88f573cf3cc919c6f906
guoguang0536@gmail.com <guoguang0536@gmail.com>
2019-11-01 00:22:04 +0800

    :lipstick: Update style

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,23 +1,35 @@
 
 <template>
-  <span>
-    <background/>
-    <layout>
-      <main-form/>
-    </layout>
-  </span>
+  <layout>
+    <main-form/>
+  </layout>
 </template>
 
 <script>
 import Layout from './Layout/index'
 import MainForm from './Layout/form'
-import Background from './Layout/background'
 export default {
   name: 'Login',
   components: {
     Layout,
-    MainForm,
-    Background
+    MainForm
+
   }
 }
 </script>
+<style >
+body {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 0;
+  background: url(https://vue-admin-guoguang.oss-cn-shanghai.aliyuncs.com/%E7%AE%A1%E7%90%86%E7%B3%BB%E7%BB%9F%E7%99%BB%E5%BD%95%E9%A1%B5.png)
+    no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+</style>
