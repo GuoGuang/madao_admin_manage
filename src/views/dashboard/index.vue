42ac509687f4dedea04bb2e55938e62a28827bf1
guoguang0536@gmail.com <guoguang0536@gmail.com>
2020-02-16 20:25:14 +0800

    :sparkles: 添加界面引导功能

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="dashboard-container">
+  <div ref="88888" class="dashboard-container">
     <component :is="currentRole" />
   </div>
 </template>
@@ -10,22 +10,22 @@ import adminDashboard from './admin'
 import editorDashboard from './editor'
 
 // 界面引导
-// import * as Driver from 'driver.js' // 导入 driver.js
-// import 'driver.js/dist/driver.min.css' // 导入 driver.js css
+import Driver from 'driver.js' // 导入 driver.js
+import 'driver.js/dist/driver.min.css' // 导入 driver.js css
 import steps from '@/components/guide/defineSteps.js' // 步骤
-
+import { getLoginStatus } from '@/utils/auth'
 export default {
   name: 'Dashboard',
   components: { adminDashboard, editorDashboard },
   data() {
     return {
-      currentRole: 'adminDashboard'
-      // driver: new Ddriver({
-      //   doneBtnText: '完成', // Text on the final button
-      //   closeBtnText: '关闭', // Text on the close button for this step
-      //   nextBtnText: '下一步', // Next button text for this step
-      //   prevBtnText: '上一步' // Previous button text for this step
-      // })
+      currentRole: 'adminDashboard',
+      driver: new Driver({
+        doneBtnText: '完成', // Text on the final button
+        closeBtnText: '关闭', // Text on the close button for this step
+        nextBtnText: '下一步', // Next button text for this step
+        prevBtnText: '上一步' // Previous button text for this step
+      })
     }
   },
   computed: {
@@ -37,7 +37,7 @@ export default {
   },
   mounted() {
     // 用户第一次登录时启用页面引导
-    if (!this.avatar) {
+    if (!getLoginStatus()) {
       this.driver.defineSteps(steps)
       this.driver.start()
     }
