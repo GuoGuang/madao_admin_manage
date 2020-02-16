1a26381b4b88d958679150a0d560792e6e77f62e
guoguang0536@gmail.com <guoguang0536@gmail.com>
2020-02-16 20:24:35 +0800

    :art: 优化首页图表宽度

--- src/components/Charts/roughviz/RoughStackedBar.vue
+++ src/components/Charts/roughviz/RoughStackedBar.vue
@@ -12,6 +12,8 @@ export default {
   mounted() {
     const stackedBarAttr = this.putAttrsToObj
     stackedBarAttr.element = '#stacked-bar'
+    // TODO 优化为外部传入
+    stackedBarAttr.width = document.getElementById('stacked').clientWidth - 50
     const stackedBar = new StackedBar(stackedBarAttr)
     return stackedBar
   }
