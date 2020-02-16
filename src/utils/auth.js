a66188a4642f411589823cfdc17ed144e207f9a1
guoguang0536@gmail.com <guoguang0536@gmail.com>
2020-02-16 20:26:43 +0800

    :alien: 添加界面引导接口

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,6 +1,7 @@
 import Cookies from 'js-cookie'
 
 const TokenKey = 'Admin-Token'
+const loginStatus = 'login-status'
 
 export function getToken() {
   return Cookies.get(TokenKey)
@@ -12,4 +13,18 @@ export function setToken(token) {
 
 export function removeToken() {
   return Cookies.remove(TokenKey)
+}
+export function getLoginStatus() {
+  return Cookies.get(loginStatus)
+}
+
+/**
+ * 是否第一次登陆
+ */
+export function isFirst() {
+  const isFirst = Cookies.get(loginStatus)
+  if (!isFirst) {
+    Cookies.set(loginStatus, true)
+  }
+  return isFirst
 }
