a05f39ee58bb8e6ececc229184b1dfb0394b8368
guoguang0536@gmail.com <guoguang0536@gmail.com>
2020-05-19 23:29:26 +0800

    :alien: 修改网站标题

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ function resolve(dir) {
   return path.join(__dirname, dir)
 }
 
-const name = defaultSettings.title || 'codeway-manager' // page title
+const name = defaultSettings.title || '码道_后台管理' // page title
 
 // If your port is set to 80,
 // use administrator privileges to execute the command line.
