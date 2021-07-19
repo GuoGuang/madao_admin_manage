a8508a3752952bf1cc3d615a66f77763a0df0158
guoguang0536@gmail.com <guoguang0536@gmail.com>
2021-07-20 02:12:25 +0800

    :bug: 修复与oauth接口重复的问题

--- src/api/user/user.js
+++ src/api/user/user.js
@@ -64,7 +64,7 @@ export function loginByGitHub(userInfo) {
  */
 export function logout() {
   return request({
-    url: '/oauth/logout',
+    url: '/oauth/user/logout',
     method: 'post'
   })
 }
